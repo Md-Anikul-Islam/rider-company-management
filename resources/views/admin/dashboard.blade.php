b94c0ed45ea167f7d73ad422608f7c3460030dcf
MD ANIKUL ISLAM <anikul.islam01@northsouth.edu>
2024-07-03 18:24:33 +0600

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -17,46 +17,96 @@
     <div class="post d-flex flex-column-fluid mb-5" id="kt_post">
         <div id="kt_content_container" class="container-fluid">
             <div class="row g-5 gx-xl-10 mb-5 mb-xl-10" >
-                <div class="col-12 col-sm-6 col-md-3" >
-                    <div class="card card-flush admin_card_style" style="background-color: red">
-                        <div class="card-header">
-                            <div class="card-title d-flex flex-column">
-                                <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{$totalCompanies}}</span>
-                                <span class="text-white opacity-100 pt-1 fw-semibold fs-2">Total Company</span>
+
+
+                <div class="col-xl-3">
+                    <div class="card card-flush h-xl-100 company_card" style="background-color: black">
+                        <div class="card-header pt-7 mb-3">
+                            <h3 class="card-title align-items-start flex-column" >
+                                <span class="card-label fw-bold text-gray-800">Total Company</span>
+
+                            </h3>
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex align-items-center me-5">
+                                    <div class="me-5">
+                                        <a href="#">{{$totalCompanies}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-				<div class="col-12 col-sm-6 col-md-3">
-                    <div class="card card-flush admin_card_style" style="background-color: red">
-                        <div class="card-header">
-                            <div class="card-title d-flex flex-column">
-                                <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{$totalPassengers}}</span>
-                                <span class="text-white opacity-100 pt-1 fw-semibold fs-2">Total Passenger</span>
+
+
+
+                <div class="col-xl-3">
+                    <div class="card card-flush h-xl-100 company_card" style="background-color: black">
+                        <div class="card-header pt-7 mb-3">
+                            <h3 class="card-title align-items-start flex-column" >
+                                <span class="card-label fw-bold text-gray-800">Total Passenger</span>
+
+                            </h3>
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex align-items-center me-5">
+                                    <div class="me-5">
+                                        <a href="#">{{$totalPassengers}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-				<div class="col-12 col-sm-6 col-md-3">
-                    <div class="card card-flush admin_card_style" style="background-color: red">
-                        <div class="card-header" >
-                            <div class="card-title d-flex flex-column">
-                                <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{$totalDrivers}}</span>
-                                <span class="text-white opacity-100 pt-1 fw-semibold fs-2">Total Driver</span>
+
+
+
+                <div class="col-xl-3">
+                    <div class="card card-flush h-xl-100 company_card" style="background-color: black">
+                        <div class="card-header pt-7 mb-3">
+                            <h3 class="card-title align-items-start flex-column" >
+                                <span class="card-label fw-bold text-gray-800">Total Driver</span>
+
+                            </h3>
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex align-items-center me-5">
+                                    <div class="me-5">
+                                        <a href="#">{{$totalDrivers}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-				<div class="col-12 col-sm-6 col-md-3">
-                    <div class="card card-flush admin_card_style" style="background-color: red">
-                        <div class="card-header">
-                            <div class="card-title d-flex flex-column">
-                                <span class="fs-2hx fw-bold text-white me-2 lh-1 ls-n2">{{$totalAgents}}</span>
-                                <span class="text-white opacity-100 pt-1 fw-semibold fs-2">Total Agent</span>
+
+
+                <div class="col-xl-3">
+                    <div class="card card-flush h-xl-100 company_card" style="background-color: black">
+                        <div class="card-header pt-7 mb-3">
+                            <h3 class="card-title align-items-start flex-column" >
+                                <span class="card-label fw-bold text-gray-800">Total Agent</span>
+
+                            </h3>
+                        </div>
+                        <div class="card-body pt-0">
+                            <div class="d-flex flex-stack">
+                                <div class="d-flex align-items-center me-5">
+                                    <div class="me-5">
+                                        <a href="#">{{$totalAgents}}</a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
+
+
+
+
             </div>
         </div>
     </div>
